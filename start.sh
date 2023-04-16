05ffeb64ded64b2f2ffe728d6c1339126c40bf1d
Toxiic-vQ <113723732+Toxiic-vQ@users.noreply.github.com>
2023-04-16 18:53:13 +0200

    auf allen Interfaces lauschen

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 #app.py als Datei fuer Flask definieren
 export FLASK_APP=app.py
 #Flask starten im Debug-Modus mit pipenv
+# mit -h 0.0.0.0 wird der Server auf allen Netzwerk-Interfaces lauschen -> wichtig fuer Docker
 pipenv run flask --debug run -h 0.0.0.0 -p 5000
