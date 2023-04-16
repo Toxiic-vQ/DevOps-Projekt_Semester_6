b652c07a66cbc103aa4759ee2f50a979b350bcbf
Toxiic-vQ <113723732+Toxiic-vQ@users.noreply.github.com>
2023-04-16 18:51:20 +0200

    Start-Skript + Docker Entrypoint angepasst

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#application.py als Datei fuer Flask definieren
-export FLASK_APP=application.py
+#app.py als Datei fuer Flask definieren
+export FLASK_APP=app.py
 #Flask starten im Debug-Modus mit pipenv
-pipenv run flask --debug run
+pipenv run flask --debug run -h 0.0.0.0 -p 5000
